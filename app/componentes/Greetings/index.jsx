3aa0ca20d526b3834b8b9e408df6833295930ab8
petrinidias <petrini4u@gmail.com>
2024-06-28 16:26:14 -0300

    Responsividade Projeto

--- app/componentes/Greetings/index.jsx
+++ app/componentes/Greetings/index.jsx
@@ -14,7 +14,7 @@ export default function Greetings() {
             <div className={Styles.greetings}>
                 <div className={Styles.texts}>
                     <p className={Styles.p1}>Olá! Me chamo Gilberto Petrini.</p>
-                    <p className={Styles.p1}>Eu desenvolvo sistemas <span className={Styles.highlight1}>Web Front-End</span>.</p>
+                    <p className={Styles.p1p2}>Eu desenvolvo sistemas <span className={Styles.highlight1}>Web Front-End</span>.</p>
                     <p className={Styles.p2}>Esse é meu portfólio! Navegue e conheça meus <Link href="#projetos" className={Styles.link}>projetos</Link> e <Link href="/sobreMim" className={Styles.link}>habilidades</Link>.</p>
                 </div>
 
