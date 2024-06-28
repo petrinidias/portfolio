4ccede536cb9be97ae438ba602dade79c729a803
petrinidias <petrini4u@gmail.com>
2024-06-28 16:39:38 -0300

    Responsividade Projeto

--- app/componentes/Greetings/index.jsx
+++ app/componentes/Greetings/index.jsx
@@ -6,6 +6,7 @@ import { FaWhatsappSquare } from "react-icons/fa";
 import { FaFile } from "react-icons/fa";
 import { MdOutlineKeyboardDoubleArrowDown } from "react-icons/md";
 import Link from "next/link"
+import { Analytics } from "@vercel/analytics/react"
 
 export default function Greetings() {
 
@@ -13,7 +14,7 @@ export default function Greetings() {
         <div className={Styles.principalG}>
             <div className={Styles.greetings}>
                 <div className={Styles.texts}>
-                    <p className={Styles.p1}>Olá! Me chamo Gilberto Petrini.</p>
+                    <p className={Styles.p1}>Olá! <Analytics/> Me chamo Gilberto Petrini.</p>
                     <p className={Styles.p1p2}>Eu desenvolvo sistemas <span className={Styles.highlight1}>Web Front-End</span>.</p>
                     <p className={Styles.p2}>Esse é meu portfólio! Navegue e conheça meus <Link href="#projetos" className={Styles.link}>projetos</Link> e <Link href="/sobreMim" className={Styles.link}>habilidades</Link>.</p>
                 </div>
