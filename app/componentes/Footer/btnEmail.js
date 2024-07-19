39dbd8f0d1d0c5d9e6b62229fdd6599af34f7179
petrinidias <petrini4u@gmail.com>
2024-07-19 19:56:38 -0300

    Uso de Toastfy

--- app/componentes/Footer/btnEmail.js
+++ app/componentes/Footer/btnEmail.js
@@ -2,13 +2,26 @@
 
 import Styles from "./Footer.module.css";
 import { IoCopySharp } from "react-icons/io5";
+import Toastify from 'toastify-js'
 
 const CopyEmailButton = () => {
     const email = 'petrini4u@gmail.com';
-  
     const copyToClipboard = () => {
       navigator.clipboard.writeText(email).then(() => {
-        alert('Meu email foi copiado para sua área de transferência!');
+
+        Toastify({
+          text: "Email copiado para área de transferência!",
+          duration: 3000,
+          close: true,
+          gravity: "top", // `top` or `bottom`
+          position: "center", // `left`, `center` or `right`
+          stopOnFocus: false, // Prevents dismissing of toast on hover
+          style: {
+            background: "var(--main)", color: "var(--primary-color)",
+          },
+          onClick: function(){} // Callback after click
+        }).showToast();
+
       }).catch(err => {
         console.error(err);
       });
